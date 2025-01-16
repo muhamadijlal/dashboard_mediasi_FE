a34a1b12bb02c3a710a0bfd7e5602e3392dcba63
idarijul <m.haidarijl@gmail.com>
2025-01-16 14:01:06 +0700

    20250116/config/set port host to 5174

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
   define: {
     env: process.env,
   },
+  server: {
+    port: 5174,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
